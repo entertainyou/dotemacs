6e1dceda6add7d756d4bd9a68312d83ed971dfda
HUANG Wei <weih@opera.com>
2012-03-06 12:02:40 +0800

    Add configs from online dotconifgs

--- .emacs.d/weih/weih-basic.el
+++ .emacs.d/weih/weih-basic.el
@@ -1,5 +1,4 @@
 ;; HUANG Wei's emacs setting
-
 (setq user-full-name "entertainyou")
 (setq user-mail-address "grubbyfans@gmail.com")
 
@@ -66,6 +65,7 @@ there's a region, all lines that region covers will be duplicated."
       (goto-char (+ origin (* (length region) arg) arg))))
   (message "line(s) are duplicated"))
 
+(global-set-key (kbd "C-c f") 'find-grep)
 (global-set-key (kbd "C-c d") 'duplicate-current-line-or-region)
 (global-set-key (kbd "C-c l") 'goto-line)
 (global-set-key (kbd "C-c m") 'menu-bar-mode)
@@ -139,6 +139,111 @@ This one changes the cursor color on each blink. Define colors in `blink-cursor-
 
 ;; handle "same name" buffers
 (require 'uniquify)
-(setq uniquify-buffer-name-style 'post-forward-angle-brackets) 
+;; (setq uniquify-buffer-name-style 'post-forward-angle-brackets)
+(setq uniquify-buffer-name-style 'forward)
+
+(defun move-line (n)
+  "Move the current line up or down by N lines."
+  (interactive "p")
+  (let ((col (current-column))
+        start
+        end)
+    (beginning-of-line)
+    (setq start (point))
+    (end-of-line)
+    (forward-char)
+    (setq end (point))
+    (let ((line-text (delete-and-extract-region start end)))
+      (forward-line n)
+      (insert line-text)
+      ;; restore point to original column in moved line
+      (forward-line -1)
+      (forward-char col))))
+
+(defun move-line-up (n)
+  "Move the current line up by N lines."
+  (interactive "p")
+  (move-line (if (null n) -1 (- n))))
+
+(defun move-line-down (n)
+  "Move the current line down by N lines."
+  (interactive "p")
+  (move-line (if (null n) 1 n)))
+
+(global-set-key (kbd "<M-up>") 'move-line-up)
+(global-set-key (kbd "<M-down>") 'move-line-down)
+
+;; ignore case when reading a file name
+(setq read-file-name-completion-ignore-case t)
+
+;; ignore case when reading a buffer name
+(setq read-buffer-completion-ignore-case t)
+
+
+(defadvice yank (after indent-region activate)
+  (if (member major-mode
+              '(emacs-lisp-mode scheme-mode lisp-mode c-mode c++-mode
+                objc-mode latex-mode plain-tex-mode python-mode))
+      (indent-region (region-beginning) (region-end) nil)))
+
+(defadvice yank-pop (after indent-region activate)
+  (if (member major-mode
+              '(emacs-lisp-mode scheme-mode lisp-mode c-mode c++-mode
+                objc-mode latex-mode plain-tex-mode python-mode))
+      (indent-region (region-beginning) (region-end) nil)))
+
+(add-hook 'autoconf-mode-hook   'flyspell-prog-mode)
+(add-hook 'autotest-mode-hook   'flyspell-prog-mode)
+(add-hook 'c++-mode-hook        'flyspell-prog-mode)
+(add-hook 'c-mode-hook          'flyspell-prog-mode)
+(add-hook 'cperl-mode-hook      'flyspell-prog-mode)
+(add-hook 'emacs-lisp-mode-hook 'flyspell-prog-mode)
+(add-hook 'makefile-mode-hook   'flyspell-prog-mode)
+(add-hook 'nxml-mode-hook       'flyspell-prog-mode)
+(add-hook 'python-mode-hook     'flyspell-prog-mode)
+(add-hook 'sh-mode-hook         'flyspell-prog-mode)
+
+;; If you want to check the result each time before repeating, then
+;; `C-x e e e...'.
+;; If you want to repeat only N times, then `C-u N C-x e'.
+;; If you want to repeat forever or until error, then `C-u 0 C-x e'.
+
+;; <shift>-<F8>  to start recording
+;; <shift>-<F8>  again to stop recording
+;; <F8>          to call it
+
+(defun my-toggle-kbd-macro-recording-on ()
+  "Start recording a keyboard macro and toggle functionality of key binding."
+  (interactive)
+  (global-set-key (kbd "<S-f8>") 'my-toggle-kbd-macro-recording-off)
+  (start-kbd-macro nil))
+
+(defun my-toggle-kbd-macro-recording-off ()
+  "Stop recording a keyboard macro and toggle functionality of key binding."
+  (interactive)
+  (global-set-key (kbd "<S-f8>") 'my-toggle-kbd-macro-recording-on)
+  (end-kbd-macro))
+
+;; start/stop recording a keyboard macro
+(global-set-key (kbd "<S-f8>") 'my-toggle-kbd-macro-recording-on)
+
+;; execute the most recent keyboard macro
+(global-set-key (kbd "<f8>") 'call-last-kbd-macro)
+
+;; auto-save every 100 input events
+(setq auto-save-interval 100)
+
+;; auto-save after 15 seconds idle time
+(setq auto-save-timeout 15)
+
+
+;; always use copying to create backup files (don't clobber symlinks)
+(setq backup-by-copying t)
+
+(remove-hook 'find-file-hooks 'vc-find-file-hook)
+
+;; load generic modes which support e.g. batch files
+(require 'generic-x)
+
 (provide 'weih-basic)
 
