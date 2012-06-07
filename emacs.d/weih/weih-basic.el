343451acc239ee916ca87785bf49b4bcee566320
HUANG Wei <weih@opera.com>
2012-06-07 17:07:47 +0800

    remove comments

--- emacs.d/weih/weih-basic.el
+++ emacs.d/weih/weih-basic.el
@@ -61,27 +61,6 @@
 
 (column-number-mode t)
 
-;; (defun duplicate-current-line-or-region (arg)
-;;   "Duplicates the current line or region ARG times.
-;; If there's no region, the current line will be duplicated. However, if
-;; there's a region, all lines that region covers will be duplicated."
-;;   (interactive "p")
-;;   (let (beg end (origin (point)))
-;;     (if (and mark-active (> (point) (mark)))
-;;         (exchange-point-and-mark))
-;;     (setq beg (line-beginning-position))
-;;     (if mark-active
-;;         (exchange-point-and-mark))
-;;     (setq end (line-end-position))
-;;     (let ((region (buffer-substring-no-properties beg end)))
-;;       (dotimes (i arg)
-;;         (goto-char end)
-;;         (newline)
-;;         (insert region)
-;;         (setq end (point)))
-;;       (goto-char (+ origin (* (length region) arg) arg))))
-;;   (message "line(s) are duplicated"))
-
 (global-set-key (kbd "C-c f") 'find-grep)
 
 (when (require 'duplicate-thing nil t)
@@ -96,7 +75,6 @@
 (global-set-key (kbd "C-c r") 'replace-regexp)
 
 (global-set-key [(f2)] 'emacs-lisp-byte-compile)
-
 
 (require 'highlight-symbol)
 (global-set-key (kbd "C-c h h") 'highlight-symbol-at-point)
