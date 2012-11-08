6702da3df4cf07a1cc47e00f8ae9e1ef8b82e788
HUANG Wei <weih@opera.com>
2012-11-08 17:26:14 +0800

    Fix blog.

--- emacs.d/weih/weih-blog.el
+++ emacs.d/weih/weih-blog.el
@@ -2,8 +2,9 @@
 
 (setq org2blog/wp-blog-alist
       '(("entertainyou"
-         :url "http://entertainyou.oicp.net/wordpress/xmlrpc.php"
-         :username "entertainyou"
+         ;; :url "http://entertainyou.oicp.net/wordpress/xmlrpc.php"
+         :url "http://www.entertainyou.us/wordpress/xmlrpc.php"
+         :username "weih"
          )))
 
 
