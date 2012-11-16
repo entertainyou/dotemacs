8507d1d1e45384c64e4bc6d4cee3488163be8b0c
HUANG Wei <weih@opera.com>
2012-11-16 15:44:01 +0800

    fix wordpress url.

--- emacs.d/weih/weih-blog.el
+++ emacs.d/weih/weih-blog.el
@@ -3,7 +3,7 @@
 (setq org2blog/wp-blog-alist
       '(("entertainyou"
          ;; :url "http://entertainyou.oicp.net/wordpress/xmlrpc.php"
-         :url "http://www.entertainyou.us/wordpress/xmlrpc.php"
+         :url "http://entertainyou.us/wordpress/xmlrpc.php"
          :username "weih"
          )))
 
