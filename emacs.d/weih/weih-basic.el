de9e7c078a5655028af1d3b67b55be3482146a01
HUANG Wei <weih@opera.com>
2013-03-12 12:59:18 +0800

    Tune emacs save file folder.

--- emacs.d/weih/weih-basic.el
+++ emacs.d/weih/weih-basic.el
@@ -226,7 +226,8 @@ This one changes the cursor color on each blink. Define colors in `blink-cursor-
  delete-old-versions t
  kept-new-versions 6
  kept-old-versions 2
- version-control t)       ; use versioned backups
+ version-control t
+ auto-save-file-name-transforms '((".*" "~/.saves/\\1" t)))       ; use versioned backups
 
 (if (require 'dired-isearch nil t)
   (progn
