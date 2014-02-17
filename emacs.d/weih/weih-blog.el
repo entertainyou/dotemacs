172db92d6fd19f0fe666ab2318c182d56c94cd27
HUANG Wei <weih@opera.com>
2014-02-18 00:12:48 +0800

    Octopress.

--- emacs.d/weih/weih-blog.el
+++ emacs.d/weih/weih-blog.el
@@ -7,8 +7,17 @@
 ;;              :url "http://entertainyou.us/wordpress/xmlrpc.php"
 ;;              :username "weih"
 ;;              ))))
-(add-to-list 'load-path "~/o-blog/")
-(try-require 'o-blog)
-(try-require 'org)
-(try-require 'org-export)
+;; (add-to-list 'load-path "~/o-blog/")
+;; (try-require 'o-blog)
+;; (try-require 'org)
+;; (try-require 'org-export)
+;; (provide 'weih-blog)
+
+(try-require 'org-octopress
+             (setq org-octopress-directory-top       "~/source/octopress/source")
+             (setq org-octopress-directory-posts     "~/source/octopress/source/_posts")
+             (setq org-octopress-directory-org-top   "~/source/octopress/source")
+             (setq org-octopress-directory-org-posts "~/source/octopress/source/blog")
+             (setq org-octopress-setup-file          "~/.emacs.d/weih/setupfile.org"))
+
 (provide 'weih-blog)
